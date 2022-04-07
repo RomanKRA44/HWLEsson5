7dd42c31f69099e33fdb8e9b9cbdccc886cf41b8
RomanKRA44 <adm-nppl@yandex.ru>
2022-04-07 19:34:11 +0300

    ДЗ 2 к уроку 5

--- HWLEsson5/Program.cs
+++ HWLEsson5/Program.cs
@@ -7,14 +7,7 @@ namespace HWLEsson5
     {
         static void Main(string[] args)
         {
-            string text, way;
-            Console.WriteLine("Вводите текст: ");
-            text = Console.ReadLine();
-            Console.WriteLine("Введите путь и имя для сохранения (в формате D:\text.txt:)");
-            way = Console.ReadLine();
-            File.WriteAllText(way, text);
-            Console.WriteLine("Успешное сохранение");
-            Console.ReadKey();
+            File.WriteAllText(@"D:\startup.txt", DateTime.Now.ToString());
         }
     }
 }
