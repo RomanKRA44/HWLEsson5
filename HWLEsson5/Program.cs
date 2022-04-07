a156ae0a6a3660e07167780e7144d6bb00f0f790
RomanKRA44 <adm-nppl@yandex.ru>
2022-04-07 19:31:58 +0300

    Добавьте файлы проекта.

---
+++ HWLEsson5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HWLEsson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
